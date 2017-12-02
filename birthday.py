dc93703539d4a1e0275b2bf5ef5654ea050dc8c8
pallavi-ghimire <pallavi.ghimire.39@gmail.com>
2017-12-02 22:44:29 +0545

    happy birthday

---
+++ birthday.py
@@ -0,0 +1,11 @@
+birthday = input("Is it your birthday? y/n: ")
+if(birthday == 'y'):
+    name = input('Name of your friend: ')
+    print('Happy birthday to you')
+    print('Happy birthday to you')
+    print('Happy birthday dear',name)
+    print('Happy birthday to you')
+elif(birthday == 'n'):
+    print("Come again on your birthday")
+else:
+    print("Wrong input")
